b2a989480cd9fb22f2378611c7b6746aec522888
RM147 <43373305+RM147@users.noreply.github.com>
2019-03-20 17:20:43 +0000

    working functions

---
+++ src/main/java/com/qa/PrizesApplication.java
@@ -0,0 +1,18 @@
+package com.qa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+@SpringBootApplication
+public class PrizesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrizesApplication.class, args);
+	}
+	
+	
+    
+
+}
